224a4d2f9f39d67ea60c00f5832b50905f6e6440
shadowpounce <minxkun@gmail.com>
2024-01-26 17:11:45 +0200

    up

--- src/widgets/Screens/Persona/data.ts
+++ src/widgets/Screens/Persona/data.ts
@@ -22,19 +22,19 @@ export const personaOneBlocks = [
         title: 'Quick and easy integration',
         subtitle:
           'Customize risk automation for swift deployment and minimize integration time.',
-        graph: '/public/images/Persona/One/1.svg',
+        graph: '/images/Persona/One/1.svg',
       },
       {
         title: 'Strategic risk analysis',
         subtitle:
           'Use a smart scoring system for simplified risk assessment, providing actionable scores to your team.',
-        graph: '/public/images/Persona/One/2.svg',
+        graph: '/images/Persona/One/2.svg',
       },
       {
         title: 'Simple customization setup',
         subtitle:
           'Customize rules easily and ensure a quick setup, reducing time and manual efforts.',
-        graph: '/public/images/Persona/One/3.svg',
+        graph: '/images/Persona/One/3.svg',
       },
     ],
   },
@@ -45,19 +45,19 @@ export const personaOneBlocks = [
         title: 'Informed decision-making',
         subtitle:
           'Use an friendly interface that breaks down risk scores, flags cases and suggests further information.',
-        graph: '/public/images/Persona/One/4.svg',
+        graph: '/images/Persona/One/4.svg',
       },
       {
         title: 'Customizable workflows',
         subtitle:
           'Beaks down risk scores, flags cases and suggests further information for informed decision-making.',
-        graph: '/public/images/Persona/One/5.svg',
+        graph: '/images/Persona/One/5.svg',
       },
       {
         title: 'Secure tracking',
         subtitle: `Secure tracking, automate communication, and guide outreach for flagged cases during renewals.
           `,
-        graph: '/public/images/Persona/One/6.svg',
+        graph: '/images/Persona/One/6.svg',
       },
     ],
   },
@@ -68,18 +68,18 @@ export const personaOneBlocks = [
         title: 'Data handling',
         subtitle:
           'Orchestrate global client re-verification to enhance rates and reduce vendor expenses costs for revenue growth.',
-        graph: '/public/images/Persona/One/7.svg',
+        graph: '/images/Persona/One/7.svg',
       },
       {
         title: 'Global coverage',
         subtitle:
           'Elevate customer verification with extensive coverage to match customer details against trusted vendors.',
-        graph: '/public/images/Persona/One/8.svg',
+        graph: '/images/Persona/One/8.svg',
       },
       {
         title: 'Proactive risk mitigation',
         subtitle: `Actively reduce risks through vendor coordination, ensuring compliance in both private and corporate assessments.`,
-        graph: '/public/images/Persona/One/9.svg',
+        graph: '/images/Persona/One/9.svg',
       },
     ],
   },
@@ -93,19 +93,19 @@ export const personaTwoBlocks = [
         title: 'Real-time collaboration',
         subtitle:
           'Centralize and automate KYC/AML processes for improved information sharing through real-time collaboration tools.',
-        graph: '/public/images/Persona/Two/1.svg',
+        graph: '/images/Persona/Two/1.svg',
       },
       {
         title: 'Unified risk view',
         subtitle:
           'Gain a comprehensive view of your processes by centralizing all your risk and monitoring processes.',
-        graph: '/public/images/Persona/Two/2.svg',
+        graph: '/images/Persona/Two/2.svg',
       },
       {
         title: 'Strategic automation',
         subtitle:
           'Establish a control framework with process automation, reducing security risks in day-to-day operations.',
-        graph: '/public/images/Persona/Two/3.svg',
+        graph: '/images/Persona/Two/3.svg',
       },
     ],
   },
@@ -117,19 +117,19 @@ export const personaTwoBlocks = [
         title: 'Smart analysis',
         subtitle:
           'Automate scenario analysis for proactive protection against financial losses and reputational damage.',
-        graph: '/public/images/Persona/Two/4.svg',
+        graph: '/images/Persona/Two/4.svg',
       },
       {
         title: 'False-positive management',
         subtitle:
           'Accelerate handling of false-positive cases through automation, freeing up time for strategic decision-making.',
-        graph: '/public/images/Persona/Two/5.svg',
+        graph: '/images/Persona/Two/5.svg',
       },
       {
         title: 'Ongoing monitoring',
         subtitle: `Reclaim lost hours by automating routine tasks, allowing focus on strategic responsibilities and cutting costs.
             `,
-        graph: '/public/images/Persona/Two/6.svg',
+        graph: '/images/Persona/Two/6.svg',
       },
     ],
   },
@@ -140,18 +140,18 @@ export const personaTwoBlocks = [
         title: 'Data silos',
         subtitle:
           'Remove information barriers, create a loop for integrated data exchange.',
-        graph: '/public/images/Persona/Two/7.svg',
+        graph: '/images/Persona/Two/7.svg',
       },
       {
         title: 'Unified control framework',
         subtitle:
           'Establish a continuous looped framework for consistent operations, minimizing security risks.',
-        graph: '/public/images/Persona/Two/8.svg',
+        graph: '/images/Persona/Two/8.svg',
       },
       {
         title: 'Automated loop reviews',
         subtitle: `Implement loops for ongoing client renewal assessments and proactive management.`,
-        graph: '/public/images/Persona/Two/9.svg',
+        graph: '/images/Persona/Two/9.svg',
       },
     ],
   },
